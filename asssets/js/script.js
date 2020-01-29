91194857f0d3057110b0eb45775396bcf389a8cc
Dung manh <dungmanh998@gmail.com>
2020-01-29 12:12:48 +0700

    optimize code

--- asssets/js/script.js
+++ asssets/js/script.js
@@ -3,74 +3,100 @@ let passwordInput = document.querySelector('.password');
 let showPasswordButton = document.querySelector('.password-button');
 let face = document.querySelector('.face')
 
-passwordInput.addEventListener('focus', function(event){
+function hideEye(){
     document.querySelectorAll('.hand').forEach(hand => {
         hand.classList.add('hide');
     });
+}
+function breath(){
+    document.querySelector('.tongue').classList.add('breath');
+}
+function holdBreath(){
     document.querySelector('.tongue').classList.remove('breath');
-});
-
-passwordInput.addEventListener('blur', function(event){
+}
+function openEye(){
     document.querySelectorAll('.hand').forEach(hand => {
         hand.classList.remove('hide');
         hand.classList.remove('peek');
+        hand.classList.remove('hang');
     });
-    document.querySelector('.tongue').classList.add('breath');
-});
-
-userNameInput.addEventListener('focus', event => {
-    document.querySelectorAll('.hand').forEach(hand => {
-        hand.classList.remove('hide'); 
-        hand.classList.remove('peek'); 
-    })
-    let length = userNameInput.value.length;
-    let faceDeg =  Math.min(length - 16 , 19);
+}
+function rotateHead(faceDeg){
     face.style.setProperty('--rotate-head',`${-faceDeg}deg`);
-});
-userNameInput.addEventListener('blur', event => {
-    face.style.setProperty('--rotate-head','0deg');
-    document.querySelectorAll('.hand').forEach(hand => {
-        hand.classList.remove('hang'); 
-    })
-});
-userNameInput.addEventListener('input', event => {
-    
-    
-    let length = userNameInput.value.length;
-    let faceDeg =  Math.min(length - 16 , 19);
+}
+function checkHand(length){
+    if(length==0){
+        document.querySelectorAll('.hand--left').forEach(hand => {
+            hand.classList.remove('hang'); 
+        });
+    }
     if(length>=1 && length <15){
         document.querySelectorAll('.hand--left').forEach(hand => {
             hand.classList.add('hang'); 
-        })
+        });
         document.querySelectorAll('.hand--right').forEach(hand => {
             hand.classList.remove('hang'); 
-        })
+        });
     }
     else if(length>=15){
         document.querySelectorAll('.hand--right').forEach(hand => {
             hand.classList.add('hang'); 
         })
     }
-    face.style.setProperty('--rotate-head',`${-faceDeg}deg`);
+}
+function peek(){
+    document.querySelectorAll('.hand').forEach(hand => {
+        hand.classList.remove('hide');
+        hand.classList.remove('hang');
+        hand.classList.add('peek');
+        breath();
+    });
+}
+function coverEyes(){
+    document.querySelectorAll('.hand').forEach(hand => {
+        hand.classList.add('hide');
+        hand.classList.remove('peek');
+        hand.classList.remove('hang');
+        holdBreath();
+    });
+}
+
+passwordInput.addEventListener('focus', function(event){
+    hideEye();
+    holdBreath();
+});
+
+passwordInput.addEventListener('blur', function(event){
+    openEye();
+    breath();
+});
+
+userNameInput.addEventListener('focus', event => {
+    openEye();
+    let faceDeg =  Math.min(userNameInput.value.length - 16 , 19);
+    rotateHead(faceDeg);
+});
+
+userNameInput.addEventListener('blur', event => {
+    rotateHead(0);
+    openEye();
+});
+
+userNameInput.addEventListener('input', event => {    
+    let length = userNameInput.value.length;
+    let faceDeg =  Math.min(length - 16 , 19);
+    checkHand(length);
+    rotateHead(faceDeg);
 });
 
 showPasswordButton.addEventListener('click', event => {
     if (passwordInput.type==='text'){
         showPasswordButton.innerHTML = 'Show';
         passwordInput.type = 'password';
-        document.querySelectorAll('.hand').forEach(hand => {
-            hand.classList.add('hide');
-            hand.classList.remove('peek');
-            hand.classList.remove('hang');
-        });
+        coverEyes();
     } else if (passwordInput.type==='password'){
         showPasswordButton.innerHTML = 'Hide';
         passwordInput.type = 'text';
-        document.querySelectorAll('.hand').forEach(hand => {
-            hand.classList.remove('hide');
-            hand.classList.remove('hang');
-            hand.classList.add('peek');
-            
-        });
+        peek();
     }
 });
